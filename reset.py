e354b3ebc5399a8b0111953d3affcd6e35a56caf
emanuel wenzlaff <emanuel.wenzlaff@gmail.com>
2020-05-05 13:52:25 +0200

    tested upload everything seems to work!

--- reset.py
+++ reset.py
@@ -4,11 +4,11 @@
 from gpiozero import LED
 from time import sleep
 
-led = LED(18)
+led = LED(18) # GPIO 18 is reset pin for the AVR
 
 led.on()
 sleep(0.1)
 led.off()
-exit()
+exit()  # important top end the script in time
 
 
